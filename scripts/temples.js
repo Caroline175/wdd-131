1552f352c3ba76e461159ff76851a6a6b03fa8c8
Caroline175 <caroline.avace1@gmail.com>
2024-05-17 09:49:40 -0600

    many updates

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Update year and last modified date
+    
     const yearSpan = document.getElementById('year');
     const lastModifiedSpan = document.getElementById('last-modified');
     
@@ -7,18 +7,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const lastModified = document.lastModified;
 
     yearSpan.textContent = currentYear;
-    lastModifiedSpan.textContent = `Last Modified: ${lastModified}`;
-
-    // Hamburger menu functionality
-    const nav = document.querySelector('nav ul');
-    const hamburger = document.createElement('div');
-    hamburger.innerHTML = '&#9776;'; // Unicode for hamburger icon
-    hamburger.className = 'hamburger';
+    lastModifiedSpan.textContent = `Last Modified: ${lastModified}`; 
+   
 
     document.querySelector('header').appendChild(hamburger);
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('open');
-        hamburger.innerHTML = nav.classList.contains('open') ? '&times;' : '&#9776;'; // Toggle between hamburger and close (X) icon
+        hamburger.innerHTML = nav.classList.contains('open') ? '&times;' : '&#9776;'; 
     });
 });
