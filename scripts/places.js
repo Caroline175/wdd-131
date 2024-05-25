c509a39dd97e94ed5d878fb3ac9549fb821f489e
Caroline175 <caroline.avace1@gmail.com>
2024-05-25 13:23:18 -0600

    images

--- scripts/places.js
+++ scripts/places.js
@@ -7,12 +7,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const lastModified = new Date(document.lastModified).toLocaleDateString();
     document.getElementById('last-modified').textContent = lastModified;
 
-    
     function calculateWindChill(temp, speed) {
         return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
     }
 
-   
     function displayWindChill() {
         const windChillElement = document.getElementById('wind-chill');
         if (temperature <= 10 && windSpeed > 4.8) {
@@ -28,4 +26,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
     displayWindChill();
 });
+
 
